c22e931b9becca9eac68bdc993d23cdeb256a7be
Victor Garcia <victorgrubiodl@gmail.com>
2023-12-21 12:27:39 +0100

    feat: using mockuser

--- src/test/java/com/mycopmany/myproject/machineapi/maintenance/MaintenanceControllerIntTest.java
+++ src/test/java/com/mycopmany/myproject/machineapi/maintenance/MaintenanceControllerIntTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -69,6 +70,7 @@ class MaintenanceControllerIntTest extends AbstractIntegrationTest {
         }
 
         @Test
+        @WithMockUser(username = "username", password = "password", roles = "USER")
         void createAndGetMaintenance() throws Exception {
                 MachineToCreate machineToCreate = new MachineToCreate(123L,
                                 "model",
