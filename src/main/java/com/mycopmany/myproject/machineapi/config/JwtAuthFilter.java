d8cc89778c17ff7d06c02c4870cdf006e77f5b55
Victor Garcia <victorgrubiodl@gmail.com>
2023-12-23 17:30:24 +0100

    feat: updated context

--- src/main/java/com/mycopmany/myproject/machineapi/config/JwtAuthFilter.java
+++ src/main/java/com/mycopmany/myproject/machineapi/config/JwtAuthFilter.java
@@ -4,8 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
 
 @AllArgsConstructor
 @Component
@@ -42,7 +47,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                         userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                SecurityContext sc = SecurityContextHolder.getContext();
+                sc.setAuthentication(authToken);
+                SecurityContextHolder.setContext(sc);
             }
         }
         filterChain.doFilter(request,response);
